8c11888e8d81c968e568e9563d97b201761927b9
Tinuz <tinuzsharp@gmail.com>
2025-06-16 22:00:31 +0200

    fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import { Analytics } from "@vercel/analytics/next"
