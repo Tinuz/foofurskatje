47cfcdb5c18caf95db6065835096f6c98ce33f0d
Tinuz <tinuzsharp@gmail.com>
2025-06-20 22:06:45 +0200

    removed comments

--- src/app/shills/page.tsx
+++ src/app/shills/page.tsx
@@ -1,4 +1,3 @@
-// filepath: /Users/leeum21/Documents/Projecten/Foofur/website/foofurskatje/src/app/shills/page.tsx
 export default function ShillsPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
